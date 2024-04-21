2b86436e20b8302e890572a8501d7ef4c32ffb91
Pablo <143550869+arkngel212@users.noreply.github.com>
2024-04-21 17:38:21 +0000

    second attempt

--- src/js/component/SimpleCounterBonus.jsx
+++ src/js/component/SimpleCounterBonus.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-
+	import '../../styles/trafficlight.css';
+      
 
 export const SimpleCounterBonus = () => {
   const [counter, setCounter] = useState(0);
@@ -59,27 +60,38 @@ export const SimpleCounterBonus = () => {
 		}
 	}, [isRunning, counter, countUp]);
 
+export const TrafficLight = () => {
+  const [color, setColor] = useState("");  // 'yellow' 'green' 'red'
 
   return (
     <div className="container">
       <h1>Simple Counter</h1>
       <h2 className={status.titleStyles}>{status.title}</h2>
+	  <div className="container d-flex justify-content-around mt-5">
+      <div className={"col-2 bg-success" + (color === 'green' ? " glow-green" : '')}
+        onClick={() => setColor('green')}></div>
+      <div className={"col-2 bg-warning " + (color === 'yellow' ? 'glow-yellow' : '')}
+        onClick={() => setColor('yellow')}></div>
+      <div className={"col-2 bg-danger " + (color === 'red' ? 'glow-red' : '')}
+        onClick={() => setColor('red')}></div>
+    </div>
+  )
+
       <div className="big-counter">
         <div><i className={status.icon}></i></div>
         {/* Days */}
-        <div>{Math.floor(counter / 1000000000 % 10)}</div>
-        <div>{Math.floor(counter / 100000000 % 10)}</div>
+        <div>{Math.floor(counter / 288000000 % 10)}</div>
         <div>{':'}</div>
         {/* Hours */}
-        <div>{Math.floor(counter / 10000000 % 10)}</div>
-        <div>{Math.floor(counter / 1000000 % 10)}</div>
+        <div>{Math.floor(counter / 12000000 % 24)}</div>
+        <div>{Math.floor(counter / 1200000 % 10)}</div>
         <div>{':'}</div>
         {/* Minutes */}
-        <div>{Math.floor(counter / 100000 % 10)}</div>
-        <div>{Math.floor(counter / 10000 % 10)}</div>
+        <div>{Math.floor(counter / 60000 % 6)}</div>
+        <div>{Math.floor(counter / 6000 % 10)}</div>
         <div>{':'}</div>
         {/* Seconds */}
-        <div>{Math.floor(counter / 1000 % 10)}</div>
+        <div>{Math.floor(counter / 1000 % 6)}</div>
         <div>{Math.floor(counter / 100 % 10)}</div>
         <div>{','}</div>
         <div>{Math.floor(counter / 10 % 10)}</div>
